3af01e2854cd62810f34063562bda1016d421c19
FedeScuote <fscuoteguazza@gmail.com>
2016-02-21 17:28:50 -0300

    set the heroku setting to staging one, also commented static files since we won't be serving those

--- homtec/settings/staging.py
+++ homtec/settings/staging.py
@@ -10,21 +10,21 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-SOCIAL_AUTH_FACEBOOK_KEY = ''
-SOCIAL_AUTH_FACEBOOK_SECRET = ''
-
-SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
-  'locale': 'en_US',
-  'fields': 'id, name, email, age_range'
-}
-
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+# # Static asset configuration
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+#
+# SOCIAL_AUTH_FACEBOOK_KEY = ''
+# SOCIAL_AUTH_FACEBOOK_SECRET = ''
+#
+# SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+#   'locale': 'en_US',
+#   'fields': 'id, name, email, age_range'
+# }
+#
+# SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
